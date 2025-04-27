1f74356cc96866ad8e90eb85cf814a89012c0b26
Evan Prodromou <evan@socialwebfoundation.org>
2025-04-27 14:00:59 -0400

    fix: require() statements

--- index.js
+++ index.js
@@ -16,9 +16,9 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 const { BigQuery } = require('@google-cloud/bigquery');
-const { fs } = require('fs').promises;
-const { path } = require('path');
-const { marked } = require('marked');
+const fs = require('fs').promises;
+const path = require('path');
+const marked = require('marked');
 
 const bq = new BigQuery();
 
